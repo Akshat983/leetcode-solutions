f8f139a7ff198db4b849149fee1aa5706ef9a565
Akshat Agarwal <408akshat@gmail.com>
2026-07-09 09:26:14 +0000

    Code challenge submission for question: 172 Factorial Trailing Zeroes

---
+++ 172 Factorial Trailing Zeroes/172factorial-trailing-zeroes.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int t = 0;
+        while(n > 0) {
+            n = n/5;
+            t+=n;
+        }
+        return t;
+    }
+}

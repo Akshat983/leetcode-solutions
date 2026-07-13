19b8411fdb6e909e84cda7d1f3fc74020ca6cb19
Akshat Agarwal <408akshat@gmail.com>
2026-07-13 20:10:41 +0000

    Code challenge submission for question: 633 Sum of Square Numbers

---
+++ 633 Sum of Square Numbers/633sum-of-square-numbers.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int i = 0;
+        int j = (int) Math.sqrt(c);
+        while (i <= j) {
+            long sum = (long) i * i + (long) j * j;
+            if (sum == c) return true;
+            if (sum < c) i++;
+            else j--;
+        }
+        return false;
+    }
+}

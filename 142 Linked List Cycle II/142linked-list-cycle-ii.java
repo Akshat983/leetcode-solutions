2ce80d47c722721907c15266efa8d8034e793af7
Akshat Agarwal <408akshat@gmail.com>
2026-07-24 21:29:23 +0000

    Code challenge submission for question: 142 Linked List Cycle II

---
+++ 142 Linked List Cycle II/142linked-list-cycle-ii.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        ListNode fp = head;
+        ListNode sp = head;
+        while(fp != null && fp.next != null) {
+            fp = fp.next.next;
+            sp = sp.next;
+            if(sp == fp) {
+                sp = head;
+                while(fp != sp) {
+                    fp = fp.next;
+                    sp = sp.next;
+                }
+                return fp;
+            }
+        }
+        return null;
+    }
+}

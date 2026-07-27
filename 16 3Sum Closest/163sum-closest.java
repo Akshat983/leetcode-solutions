a583d0830c11c923947a5b329578338834046098
Akshat Agarwal <408akshat@gmail.com>
2026-07-27 09:36:03 +0000

    Code challenge submission for question: 16 3Sum Closest

---
+++ 16 3Sum Closest/163sum-closest.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        for (int k = 0; k < nums.length - 2; k++) {
+            int i = k + 1;
+            int j = nums.length - 1;
+            while (i < j) {
+                int sum = nums[k] + nums[i] + nums[j];
+                if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                    closest = sum;
+                }
+                if (sum == target) {
+                    return sum;
+                } else if (sum < target) {
+                    i++;
+                } else {
+                    j--;
+                }
+            }
+        }
+        return closest;
+    }
+}

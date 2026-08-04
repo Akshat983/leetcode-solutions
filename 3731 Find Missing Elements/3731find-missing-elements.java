09f99143b16d6379ec4fd9842cdd55506ccadafa
Akshat Agarwal <408akshat@gmail.com>
2026-08-04 13:37:25 +0000

    Code challenge submission for question: 3731 Find Missing Elements

---
+++ 3731 Find Missing Elements/3731find-missing-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> l = new ArrayList<>();
+        Arrays.sort(nums);
+        int j = 0;
+        for(int i = nums[0]; i <= nums[nums.length-1]; i++) {
+            if(nums[j] == i) {
+                j++;
+                continue;
+            }
+            l.add(i);
+        }
+        return l;
+    }
+}
